082e91d59d749bb5da606eb811c518d91da3f2b3
Jia Tian <klbjlabs@gmail.com>
2020-06-12 15:56:02 +0800

    update

--- Week01/1.two_sum.go
+++ Week01/1.two_sum.go
@@ -5,17 +5,30 @@ import (
 )
 
 func main() {
-	xx := map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
-		"four":  4,
-		"five":  5,
-	}
-	// ages := make(map[string]int)
-	// delete(ages, "alice")
+	nums := [...]int{2, 7, 11, 15}
+	target := 9
 
-	for k, v := range xx {
-		fmt.Println(k, v)
-	}
+	fmt.Println(twoSum(nums[:], target))
 }
+
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+	return nil
+}
+
+// func twoSum(nums []int, target int) []int {
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] == target {
+// 				return []int{i, j}
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
